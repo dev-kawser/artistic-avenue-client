28f9439f4eeb7a68d13f7de6784e7198751f678b
devsafi <mdkawserferdoussafi@gmail.com>
2024-04-28 00:46:05 +0600

    react lottie added

--- src/Routes/ProtectedRoute/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute/ProtectedRoute.jsx
@@ -2,6 +2,8 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../../Context/ContextProvider";
+import Lottie from "lottie-react";
+import animation from '../../assets/animation.json'
 
 
 const ProtectedRoute = ({ children }) => {
@@ -9,8 +11,8 @@ const ProtectedRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
 
     if (loading) {
-        return <div>
-            <span className="loading flex lg:h-[500px] justify-center items-center lg:mt-20 mx-auto loading-spinner loading-lg"></span>
+        return <div className="flex lg:h-[500px] justify-center items-center lg:mt-20 mx-auto">
+            <Lottie animationData={animation}></Lottie>
         </div>
     }
 
